80dd01bad3fc0025a46b97bcd2d333be0c2d5106
Preet Mistry <pmistry9597@gmail.com>
2023-02-19 19:07:02 -0500

    policy gradient loss + tests (unit test caught something that would have been a pain in the ass to fix

--- rl/policy_grad/trainer.py
+++ rl/policy_grad/trainer.py
@@ -1,4 +1,6 @@
 from .. import trainer
+from functools import partial
+import torch
 
 class PolicyGradTrainer(trainer.Trainer):
     def act(self, obs, trajec_no):
@@ -9,7 +11,8 @@ class PolicyGradTrainer(trainer.Trainer):
         self.trajecs[-1].append(sampl)
 
     def termin_trajectory(self):
-        # note: trigger training loop here when right
+        # note: trigger training update here when right
+        # and create new lists for recording actions, policy prob outputs, etc
         pass
 
     # --- specific to class section ---
@@ -20,3 +23,21 @@ class PolicyGradTrainer(trainer.Trainer):
         self.policy_optim = policy_optim
         self.reward_decay = reward_decay
         self.trajecs_til_update = trajecs_til_update
+
+# calculate loss for policy gradient
+# take in trajectories to compute over, advantage/reward fn of trajectory, policy probability outputs recorded, actions actually taken
+def trajec_loss(trajecs_rewards, reward_decay, advantage_fn, policy_outs, acts_taken):
+    advantages_tup = tuple(map(partial(trajec_advantage, reward_decay=reward_decay, advantage_fn=advantage_fn), trajecs_rewards))
+    advantages = torch.tensor(advantages_tup)
+    
+    policy_outs = torch.stack(policy_outs)
+    prob_of_acts = policy_outs.gather(2, acts_taken.unsqueeze(2)) # 2 is dim to select individual probabilities
+
+    # policy loss formula <- sum of(log of acts .* advantages)
+    log_act_prob = torch.log(prob_of_acts)
+    return torch.mean(log_act_prob.squeeze() * advantages)
+
+    # note: may need to update as may accept tensor instead of list of trajectory features
+
+def trajec_advantage(trajec_rewards, reward_decay, advantage_fn):
+    return tuple(map(lambda i: advantage_fn(trajec_rewards[i:], reward_decay), range(len(trajec_rewards))))
---
+++ test/policy_grad.py
@@ -0,0 +1,33 @@
+import rl.policy_grad.trainer as pol_train
+import unittest
+import torch
+from torch.nn.functional import softmax
+
+class PolicyGradTrainerTest(unittest.TestCase):
+    def test_trajec_loss(self):
+        trajec_rewards = torch.tensor([(1,-5,-69), (3,4,-5)])
+        reward_decay = 0.9
+        advantage_fn = lambda rew_traj, decay: torch.sum( decay ** torch.arange(len(rew_traj)) * rew_traj )
+
+        policy_outs = [tuple(map(lambda ten: softmax(torch.tensor(ten, dtype=torch.double), dim=0), ((3,5),(1,2),(10,4)))), tuple(map(lambda ten: softmax(torch.tensor(ten, dtype=torch.double), dim=0), ((4,3),(6,4),(7,3))))]
+        policy_outs = tuple(map(torch.stack, policy_outs))
+
+        acts_taken = torch.tensor([(0,1,0,), (1,1,0)])
+
+        actual_loss_val = pol_train.trajec_loss(trajec_rewards, reward_decay, advantage_fn, policy_outs, acts_taken)
+
+        decay_fct = reward_decay ** torch.arange(3)
+        expected_loss_seq = []
+        # self.assertGreater(0, len(trajec_rewards))
+        for full_traj, act_traj, pol_traj in zip(trajec_rewards, acts_taken, policy_outs):
+            # self.assertGreater(0, len(full_traj))
+            for i in range(len(full_traj)):
+                traj = full_traj[-(i+1):]
+                adv = torch.sum( traj * decay_fct[:i+1] )
+                act = act_traj[-(i+1)]
+                log_pol = torch.log(pol_traj[-(i+1),act])
+                loss_single = log_pol * adv
+                expected_loss_seq.append(loss_single)
+        expected_loss_val = torch.mean(torch.stack(expected_loss_seq))
+        self.assertEqual(actual_loss_val, expected_loss_val)
+        # note: may need to update test as function may accept tensor instead of list of trajectory features
